ea938d74f2981e34c79a1b2425489e8a73f71523
r3g492 <kangggunw3492@gmail.com>
2024-03-23 16:46:03 +0900

    keyboard text input test

--- main.go
+++ main.go
@@ -6,10 +6,26 @@ func main() {
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	rl.SetTargetFPS(60)
 
+	var name string = ""
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		rl.DrawText("Input your name:", 190, 240, 20, rl.LightGray)
+
+		var charPressed int32 = rl.GetCharPressed()
+		if charPressed != 0 {
+			name += string(charPressed)
+		}
+
+		if rl.IsKeyPressed(rl.KeyBackspace) && len(name) > 0 {
+			name = name[:len(name)-1]
+		}
+
+		rl.DrawText(name, 190, 260, 20, rl.LightGray)
+
 		rl.EndDrawing()
 	}
 
