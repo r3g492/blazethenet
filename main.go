ecbd8d6df8ddf197dcfd459694a164b499dfc532
r3g492 <kangggunw3492@gmail.com>
2024-07-03 03:14:33 +0900

    tinkering with name input

--- main.go
+++ main.go
@@ -17,23 +17,33 @@ func main() {
 
 	// var gameState GameState = NameCreation
 
+	var nameCreated bool = false
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
-		rl.DrawText("Input your name:", 190, 240, 20, rl.LightGray)
-
 		var charPressed int32 = rl.GetCharPressed()
-		if charPressed != 0 {
-			name += string(charPressed)
-		}
 
-		if rl.IsKeyPressed(rl.KeyBackspace) && len(name) > 0 {
-			name = name[:len(name)-1]
-		}
+		if !nameCreated {
+			rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+			rl.DrawText("Input your name:", 190, 240, 20, rl.LightGray)
 
-		rl.DrawText(name, 190, 260, 20, rl.LightGray)
+			if charPressed != 0 {
+				name += string(charPressed)
+			}
+
+			if rl.IsKeyPressed(rl.KeyBackspace) && len(name) > 0 {
+				name = name[:len(name)-1]
+			}
+
+			if rl.IsKeyPressed(rl.KeyEnter) {
+				nameCreated = true
+			}
+
+			rl.DrawText(name, 190, 280, 20, rl.LightGray)
+		} else {
+			rl.DrawText("Welcome to the game, "+name, 190, 200, 20, rl.LightGray)
+		}
 
 		rl.EndDrawing()
 	}
