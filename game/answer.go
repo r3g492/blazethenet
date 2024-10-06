a6dedae8e16acaed1ca9ddd90f6075c08f41d6b7
r3g492 <kangggunw3492@gmail.com>
2024-10-06 21:42:18 +0900

    answer 관련 로직 작성

--- game/answer.go
+++ game/answer.go
@@ -4,34 +4,37 @@ type Answer struct {
 	Subject string
 	Verb    Verb
 	Object  Object
+	Found   bool
 }
 
 type Verb int
 
 const (
-	VerbFears Verb = iota
+	VerbDefault Verb = iota
+	VerbFears
 	VerbWeakAgainst
 	VerbLooksLike
 	VerbHas
 )
 
 func (v Verb) String() string {
-	return [...]string{"fears", "is weak against", "looks like", "has"}[v]
+	return [...]string{"null", "fears", "is weak against", "looks like", "has"}[v]
 }
 
 type Object int
 
 const (
-	ObjectFire Object = iota
+	ObjectDefault Object = iota
+	ObjectFire
 	ObjectPointyThings
 	ObjectWater
 	ObjectThunder
 )
 
 func (o Object) String() string {
-	return [...]string{"fire", "pointy things", "water", "thunder"}[o]
+	return [...]string{"null", "fire", "pointy things", "water", "thunder"}[o]
 }
 
-func newAnswer(verb Verb, object Object) Answer {
-	return Answer{"The Monster", verb, object}
+func newAnswer(verb Verb, object Object, found bool) Answer {
+	return Answer{"The Monster", verb, object, found}
 }
--- game/game.go
+++ game/game.go
@@ -1,26 +1,53 @@
 package game
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func Logic() bool {
 	rl.ClearBackground(rl.Black)
-
 	answers := []Answer{
-		newAnswer(VerbFears, ObjectFire),
-		newAnswer(VerbWeakAgainst, ObjectWater),
-		newAnswer(VerbLooksLike, ObjectPointyThings),
+		newAnswer(
+			VerbFears,
+			ObjectFire,
+			true,
+		),
+		newAnswer(
+			VerbWeakAgainst,
+			ObjectWater,
+			false,
+		),
+		newAnswer(
+			VerbLooksLike,
+			ObjectPointyThings,
+			false,
+		),
 	}
 
-	var y int32 = 100
+	var y int32 = 200
+	rl.DrawText(fmt.Sprintf("answer count: %d", len(answers)), 10, 50, 50, rl.Red)
 	for _, answer := range answers {
+		if !answer.Found {
+			continue
+		}
 		rl.DrawText(answer.Subject+" "+answer.Verb.String()+" "+answer.Object.String(),
 			10,
 			y,
-			100,
+			50,
 			rl.Blue,
 		)
 		y += 100
 	}
+
+	var chosenVerb = VerbDefault
+	var chosenObject = ObjectDefault
+	rl.DrawText(chosenVerb.String()+" "+chosenObject.String(),
+		400,
+		10,
+		50,
+		rl.Brown,
+	)
 
 	return true
 }
