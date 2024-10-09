b0dbd462c4b86bf30054e62887a28fe04614e8aa
r3g492 <kangggunw3492@gmail.com>
2024-10-10 02:56:59 +0900

    answer check implementation

--- game/game.go
+++ game/game.go
@@ -7,7 +7,51 @@ import (
 
 func Logic() bool {
 	rl.ClearBackground(rl.Black)
-	answers := []Answer{
+	answers := getAnswers()
+	drawAnswers(answers)
+
+	var chosenVerb = VerbDefault
+	var chosenObject = ObjectDefault
+	rl.DrawText(chosenVerb.String()+" "+chosenObject.String(),
+		400,
+		10,
+		50,
+		rl.Brown,
+	)
+
+	answerCheck := false
+
+	if answerCheck {
+		for answerIdx := range answers {
+			if answers[answerIdx].Verb == chosenVerb &&
+				answers[answerIdx].Object == chosenObject {
+				answers[answerIdx].Found = true
+			}
+		}
+	}
+
+	return true
+}
+
+func drawAnswers(answers []Answer) {
+	var y int32 = 200
+	rl.DrawText(fmt.Sprintf("answer count: %d", len(answers)), 10, 50, 50, rl.Red)
+	for _, answer := range answers {
+		if !answer.Found {
+			continue
+		}
+		rl.DrawText(answer.Subject+" "+answer.Verb.String()+" "+answer.Object.String(),
+			10,
+			y,
+			50,
+			rl.Blue,
+		)
+		y += 100
+	}
+}
+
+func getAnswers() []Answer {
+	return []Answer{
 		newAnswer(
 			VerbFears,
 			ObjectFire,
@@ -24,30 +68,4 @@ func Logic() bool {
 			false,
 		),
 	}
-
-	var y int32 = 200
-	rl.DrawText(fmt.Sprintf("answer count: %d", len(answers)), 10, 50, 50, rl.Red)
-	for _, answer := range answers {
-		if !answer.Found {
-			continue
-		}
-		rl.DrawText(answer.Subject+" "+answer.Verb.String()+" "+answer.Object.String(),
-			10,
-			y,
-			50,
-			rl.Blue,
-		)
-		y += 100
-	}
-
-	var chosenVerb = VerbDefault
-	var chosenObject = ObjectDefault
-	rl.DrawText(chosenVerb.String()+" "+chosenObject.String(),
-		400,
-		10,
-		50,
-		rl.Brown,
-	)
-
-	return true
 }
